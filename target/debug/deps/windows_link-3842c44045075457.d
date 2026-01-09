a66d0bd8c3574ef6b4155de20bbb4adc842c6dd2
Salta1414 <saltabadly@gmail.com>
2026-01-09 23:32:32 +0100

    fix: Remove build artifacts and add .gitignore

--- target/debug/deps/windows_link-3842c44045075457.d
+++
@@ -1,6 +0,0 @@
-C:\Salta\Sapo\cli\sapo-cli\target\debug\deps\windows_link-3842c44045075457.d: C:\Users\Salta\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\lib.rs C:\Users\Salta\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\../readme.md
-
-C:\Salta\Sapo\cli\sapo-cli\target\debug\deps\libwindows_link-3842c44045075457.rmeta: C:\Users\Salta\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\lib.rs C:\Users\Salta\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\../readme.md
-
-C:\Users\Salta\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\lib.rs:
-C:\Users\Salta\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\../readme.md:
